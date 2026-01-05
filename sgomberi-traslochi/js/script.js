436543c59311264a46605580dc5e2ce613aea4ea
davidnatanaeltalmaciu-netizen <davidnatanaeltalmaciu@gmail.com>
2026-01-05 16:41:29 +0100

    Add files via upload

---
+++ sgomberi-traslochi/js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
+
+console.log("Sito caricato correttamente");
